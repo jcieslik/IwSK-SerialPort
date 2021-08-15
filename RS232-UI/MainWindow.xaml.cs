aaee30b986bffebb1ddb129db9988f5c97229d89
Jakub Cieślik <jakucie936@student.polsl.pl>
2021-08-15 09:34:18 +0200

    creating UI for RS232 - WIP

--- RS232-UI/MainWindow.xaml.cs
+++ RS232-UI/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,7 @@ namespace RS232_UI
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new ViewModel();
         }
     }
 }
---
+++ RS232-UI/ViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RS232_UI
+{
+    public class ViewModel
+    {
+        public List<string> Ports { get; set; }
+
+        public ViewModel()
+        {
+            Ports = SerialPort.GetPortNames().ToList();
+        }
+    }
+}
